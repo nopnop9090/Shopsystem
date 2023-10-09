31d11f7e4df4fcfdfad8111e3394ec76d88909b3
nopnop <nopnop9090@v40.net>
2023-10-09 08:57:04 +0200

    Adresse Klassse angelegt

--- src/Shopsystem/Adresse.java
+++ src/Shopsystem/Adresse.java
@@ -1,8 +1,26 @@
 package Shopsystem;
 
 public class Adresse {
+	// Attribute
 	private String strasse;
-	private String postleitzahl;
+	private String plz;
 	private String ort;
 	
+	// Konstruktor
+	public Adresse(String strasse, String plz, String ort) {
+		super();
+		this.strasse = strasse;
+		this.plz = plzl;
+		this.ort = ort;
+	}
+
+	// formatierte Ausgabe
+	@Override
+	public String toString() {
+		// Musterstr. 7, 12345 Musterhausen
+		return strasse + ", " + plz + " " + ort;
+	}
+	
+	
+	// getter und setter brauchen wir für diese Klasse momentan nicht
 }
