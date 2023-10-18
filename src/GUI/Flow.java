0aee520ca9cbbc69c7a00898337f3bd291e47958
nopnop <nopnop9090@v40.net>
2023-10-18 10:13:54 +0200

    change: buttons ohne schleife

--- src/GUI/Flow.java
+++ src/GUI/Flow.java
@@ -11,11 +11,30 @@ public class Flow {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel(new FlowLayout());
+		
+		/*
 		for(int i=1; i<=10; i++) {
 			JButton button = new JButton("Button " + i);
 			panel.add(button);
 		}
+		*/
+		
+		JButton b1=new JButton("Button 1");
+		JButton b2=new JButton("Button 2");
+		JButton b3=new JButton("Button 3");
+		JButton b4=new JButton("Button 4");
+		JButton b5=new JButton("Button 5");
+		JButton b6=new JButton("Button 6");
+		JButton b7=new JButton("Button 7");
 
+		panel.add(b1);
+		panel.add(b2);
+		panel.add(b3);
+		panel.add(b4);
+		panel.add(b5);
+		panel.add(b6);
+		panel.add(b7);
+		
 		frame.add(panel);
 		frame.setSize(500,500);
 		frame.setLocationRelativeTo(null);		
