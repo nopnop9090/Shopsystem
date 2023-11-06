92c4196644d369a9ac799e1884a745d44833808e
nopnop <nopnop9090@v40.net>
2023-11-06 11:58:01 +0100

    Update Grid.java

--- src/GUI/Grid.java
+++ src/GUI/Grid.java
@@ -5,14 +5,14 @@ import javax.swing.*;
 
 public class Grid {
 
-	public static void main(String[] args) {
-		
-		JFrame frame = new JFrame("Beispiel Grid");
+	JFrame frame = new JFrame("Beispiel Grid");
+
+	// 3 Zeilen, 2 Spalten
+	JPanel panel = new JPanel(new GridLayout(3, 2));
+	
+	public Grid() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		// 3 Zeilen, 2 Spalten
-		JPanel panel = new JPanel(new GridLayout(3, 2));
-		
+
 		JButton btn1 = new JButton("Button 1");
 		JButton btn2 = new JButton("Button 2");
 		JButton btn3 = new JButton("Button 3");
@@ -20,19 +20,27 @@ public class Grid {
 		JButton btn5 = new JButton("Button 5");
 		JButton btn6 = new JButton("Button 6");
 		
+		btn1.addActionListener(this);
+		btn2.addActionListener(this);
+		btn3.addActionListener(this);
+		btn4.addActionListener(this);
+		btn5.addActionListener(this);
+		btn6.addActionListener(this);
+		
 		panel.add(btn1);
 		panel.add(btn2);
 		panel.add(btn3);
 		panel.add(btn4);
 		panel.add(btn5);
 		panel.add(btn6);
-	
 		
 		frame.add(panel);
 		frame.setSize(400,400);
 		frame.setLocationRelativeTo(null);		
 		frame.setVisible(true);
-		
 	}
 
+	public static void main(String[] args) {
+		new Grid();
+	}
 }
