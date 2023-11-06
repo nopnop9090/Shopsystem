653e50c5014ee1aa005117e0a9ace7703fb7c34a
nopnop <nopnop9090@v40.net>
2023-11-06 14:32:49 +0100

    Update Menu.java

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -1,10 +1,12 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class Menu {
+public class Menu implements ActionListener {
 
 	public Menu() {
 		JFrame frame = new JFrame("Beispiel Menu");
@@ -20,10 +22,14 @@ public class Menu {
 
 		// 1. Eintrag zu Datei
 		mi = new JMenuItem("Neu");
+		mi.setMnemonic('N');
+		mi.addActionListener(this);
 		menu.add(mi);
 		
 		// 2. Eintrag zu Datei
 		mi = new JMenuItem("Speichern");
+		mi.setMnemonic('S');
+		mi.addActionListener(this);
 		menu.add(mi);
 
 		// Trennlinie
@@ -34,6 +40,8 @@ public class Menu {
 		
 		// mittels classloader ist eine bessere methode um den pfad zu bilddateien zu bestimmen:  
 		mi = new JMenuItem("Beenden", new ImageIcon(getClass().getResource("/GUI/b_drop.png")));
+		mi.setMnemonic('B');
+		mi.addActionListener(this);
 		menu.add(mi);
 		
 		// 1. Eintrag "Datei" der Menubar hinzufügen
@@ -42,13 +50,16 @@ public class Menu {
 		// 2. Eintrag in der Menubar
 		menu = new JMenu("Artikel");
 		mi = new JMenuItem("Katzenkram");
+		mi.addActionListener(this);
 		menu.add(mi);
 		
 		subMenu = new JMenu("Hundekram");
 		
 		mi = new JMenuItem("Knochen");
+		mi.addActionListener(this);
 		subMenu.add(mi);
 		mi = new JMenuItem("Spielzeug");
+		mi.addActionListener(this);
 		subMenu.add(mi);
 		
 		menu.add(subMenu);
@@ -66,6 +77,19 @@ public class Menu {
 	
 	public static void main(String[] args) {
 		new Menu();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(e.getActionCommand() + " geklickt!");
+		
+		if(e.getActionCommand().equalsIgnoreCase("speichern")) {
+			System.out.println("Speichern!");
+		}
+
+		if(e.getActionCommand().equalsIgnoreCase("beenden")) {
+			System.out.println("Raus!");
+		}
 	}
 
 }
