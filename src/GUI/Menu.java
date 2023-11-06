c273f389dcddd1d7c71b12f3fcc6eb1e7311e5c7
nopnop <nopnop9090@v40.net>
2023-11-06 14:07:04 +0100

    Update Menu.java

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -30,9 +30,19 @@ public class Menu {
 		menu.addSeparator();
 		
 		// 3. Eintrag zu Datei
-		mi = new JMenuItem("Beenden");
+		//mi = new JMenuItem("Beenden", new ImageIcon("src/GUI/b_drop.png"));
+		
+		// mittels classloader ist eine bessere methode um den pfad zu bilddateien zu bestimmen:  
+		mi = new JMenuItem("Beenden", new ImageIcon(getClass().getResource("/GUI/b_drop.png")));
 		menu.add(mi);
+		
+		// 1. Eintrag "Datei" der Menubar hinzufügen
+		menuBar.add(menu);
 
+		// 2. Eintrag in der Menubar
+		menu = new JMenu("Artikel");
+
+		// 2. Eintrag "Artikel" der Menubar hinzufügen
 		menuBar.add(menu);
 		
 		frame.add(menuBar, BorderLayout.NORTH);
